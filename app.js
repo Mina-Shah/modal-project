cd0aeec62581ba2221d370e47c415175a908bef6
Mina-Shah <shahmina814@gmail.com>
2024-08-19 07:28:27 +0500

    added remove button functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // select modal-btn, modal-overlay, close-tbn
 // listen for click events on modal-btn and close-btn
 // when users click modal-btn add .OPEN-MODAL to model-overlay
+// when user clicks close-btn remove .OPEN-MODAL from modal-overlay
 
 let modalBtn = document.querySelector('.modal-btn')
 let modalOverlay = document.querySelector('.modal-overlay')
@@ -10,3 +11,6 @@ modalBtn.addEventListener('click', function() {
      modalOverlay.classList.add('open-modal')
 })
 
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal')
+    }) 
